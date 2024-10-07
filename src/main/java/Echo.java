dc2e3db582c4c24861b2549328fb2e9f168d3f4d
harshyadav02 <harshyadav5736@gmail.com>
2024-10-07 18:25:33 +0530

    error solved for null String

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 public class Echo {
-    StringBuilder sb = new StringBuilder();
 
-    public static void printMsg(String msg){
+
+    public  static  void printMsg(String msg){
 
         String msgArray[] = msg.split("[^a-zA-Z]+");
 
-        msgArray = Arrays.stream(msgArray).filter(word -> !word.isEmpty()).toArray(String[]::new);
+        String s = Arrays.stream(msgArray).filter(word -> !word.isEmpty()).toString();
 
-        System.out.println(new String(Arrays.toString(Arrays.copyOfRange(msgArray,1, msgArray.length))));
+        System.out.println(s);
     }
 }
