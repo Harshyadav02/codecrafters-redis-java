79b8b9f6259152f761878479863c2a5ec4d1d79d
harshyadav02 <harshyadav5736@gmail.com>
2024-10-07 21:40:58 +0530

    removed Echo from the message

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -6,10 +6,10 @@ public class Echo {
 
     public  static  void printMsg(String msg){
 
-        String msgArray[] = msg.split("[^a-zA-Z]+");
+        String[] msgArray = msg.split("[^a-zA-Z]+");
 
         msg = Arrays.stream(msgArray).filter(word -> !word.isEmpty()).collect(Collectors.joining(" "));
 
-        System.out.println(msg);
+        System.out.println(msg.substring(5));
     }
 }
