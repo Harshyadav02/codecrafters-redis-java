c05279e11f816ffa754afd3085d09a31e353fe11
harshyadav02 <harshyadav5736@gmail.com>
2024-10-08 09:21:51 +0530

    calling get and set method

--- src/main/java/HandleMultipleUser.java
+++ src/main/java/HandleMultipleUser.java
@@ -2,13 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class HandleMultipleUser extends Thread {
     private final Socket clientSocket;
-
+    private  Map<String,Object> map;
     // Constructor to accept client socket
-    public HandleMultipleUser(Socket clientSocket) {
+    public HandleMultipleUser(Socket clientSocket)
+    {
         this.clientSocket = clientSocket;
+        map = new HashMap<>();
     }
 
     @Override
@@ -20,6 +25,8 @@ public class HandleMultipleUser extends Thread {
             // Handle multiple commands from the client
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
+
+                System.out.println("InputLine is --> "+inputLine);
                 // Respond to PING command
                 if (inputLine.trim().equalsIgnoreCase("PING")) {
                     clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
@@ -28,9 +35,34 @@ public class HandleMultipleUser extends Thread {
                 else if ("ECHO".equalsIgnoreCase(inputLine)) {
                     in.readLine();
                     String message = in.readLine();
+                    System.out.println("message is -> "+message);
                     clientSocket.getOutputStream().write(
                             String.format("$%d\r\n%s\r\n", message.length(), message)
                                     .getBytes());
+                    clientSocket.getOutputStream().flush();
+
+                }
+
+                else if("SET".equalsIgnoreCase(inputLine)){
+                        in.readLine();
+                        String key = in.readLine();
+                        in.readLine();
+                        String value = in.readLine();
+                        GetSet.setKey(map,key,value);
+                        clientSocket.getOutputStream().write("OK\r\n".getBytes());
+                        clientSocket.getOutputStream().flush();
+                    System.out.println(map);
+                }
+                else if("GET".equalsIgnoreCase(inputLine)){
+                    in.readLine();
+                    String key = in.readLine();
+                    Object value = GetSet.getValue(map,key);
+                    if(value!=null){
+                        clientSocket.getOutputStream().write(
+                                String.format("$%d\r\n%s\r\n",value.toString().length(),value).getBytes());
+                        clientSocket.getOutputStream().flush();
+                    }
+                    System.out.println(map);
                 }
             }
         } catch (IOException e) {
