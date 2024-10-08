9b8f7d7935d545feb602a1c9d01073bf0eca0631
harshyadav02 <harshyadav5736@gmail.com>
2024-10-08 17:02:08 +0530

    added comments to the methods

--- src/main/java/GetSet.java
+++ src/main/java/GetSet.java
@@ -2,30 +2,32 @@ import java.util.HashMap;
 import java.util.Map;
 public class GetSet {
 
-
+    // method to get value based on key if key is not expired
     public static Object getValue(Map<String,Object[]> map, String key){
 
-
              Object[] value = map.get(key);
-             if(value[2] !=null) {
 
+             // check if expiry time is given or not
+             if(value[2] !=null) {
+                    // check if key got expired
                  if (checkKeyExpiry((Long) value[1], (Long)value[2])) {
 
                      return map.get(key);
-                 }else{
+                 }
+                 else{
                      map.remove(key);
                      return null;
                  }
              }
             return map.get(key);
     }
-
+    // Method for inserting the key with expiry time
     public static void setKeyWithExpiry(Map<String,Object[]> map, String key,String value, Long expiryTime){
         long insertionTimeOfKey = System.currentTimeMillis();
         map.put(key,new Object[]{value,expiryTime,insertionTimeOfKey});
     }
 
-
+    // method to check weather the key has expired or not
     private static boolean checkKeyExpiry(Long expiryTime , Long insertionTimeOfKey){
 
         return System.currentTimeMillis() - insertionTimeOfKey < expiryTime;
