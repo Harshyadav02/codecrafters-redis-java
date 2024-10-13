1d5a02468793663ba8b1ecb4d5a931575a3aea62
harshyadav02 <harshyadav5736@gmail.com>
2024-10-13 09:55:04 +0530

    Changed class name and seprated the set and get method logic

--- src/main/java/ExpiringCacheManager.java
+++ src/main/java/ExpiringCacheManager.java
@@ -1,37 +1,37 @@
 import java.util.Map;
-public class GetSet {
+public class ExpiringCacheManager{
 
     // method to get value based on key if key is not expired
-    public static Object getValue(Map<String, Object[]> map, String key) {
+    public  Object getValue(Map<String, Object[]> map, String key) {
         Object[] value = map.get(key);
+        if(value.length == 1){
 
-        if(value.length ==1){
-            return  value[0];
+            return  value[0];   // return key which has no expiry
         }
 
         // check if key got expired
         else if (checkKeyExpiry((long)value[1], (long) value[2])) {
-            //return value
-            return value[0];
+
+            return value[0]; // return value after checking expiry
         } else {
-            map.remove(key);
+            map.remove(key); // remove key if got expired
             return null;
         }
     }
     // Method for inserting the key with expiry time
-    public static void setKeyWithExpiry(Map<String,Object[]> map, String key,String value, long expiryTime){
+    public void setKeyWithExpiry(Map<String,Object[]> map, String key,String value, long expiryTime){
 
         if(expiryTime !=-1){
             long insertionTimeOfKey = System.currentTimeMillis();
             map.put(key,new Object[]{value,expiryTime,insertionTimeOfKey});
         }else{
-//            System.out.println("setting up no expiry time ");
+
             map.put(key,new Object[]{value});
         }
     }
 
     // method to check weather the key has expired or not
-    private static boolean checkKeyExpiry(long expiryTime , long insertionTimeOfKey){
+    private boolean checkKeyExpiry(long expiryTime , long insertionTimeOfKey){
 
         return System.currentTimeMillis() - insertionTimeOfKey < expiryTime;
     }
--- src/main/java/HandleMultipleUser.java
+++ src/main/java/HandleMultipleUser.java
@@ -43,53 +43,98 @@ public class HandleMultipleUser extends Thread {
 
                 }
 
-                else if("SET".equalsIgnoreCase(inputLine)){
-                        in.readLine();                      // skipping size of key --> $9
-                        String key = in.readLine();         // extracting the actual key --> raspberry
-                        in.readLine();                      // skipping size of value  --> $6
-                        String value = in.readLine();       // extracting the actual value --> banana
-                        System.out.println("GOing to read time varaible size");
-                        String sizeOfTimeVariable = in.readLine();// skipping size of time variable  --> $2
-                        System.out.println("Time variable size is "+sizeOfTimeVariable);
-                    if(sizeOfTimeVariable !=null && sizeOfTimeVariable.startsWith("$"))
+//                else if("SET".equalsIgnoreCase(inputLine)){
+//                        in.readLine();                      // skipping size of key --> $9
+//                        String key = in.readLine();         // extracting the actual key --> raspberry
+//                        in.readLine();                      // skipping size of value  --> $6
+//                        String value = in.readLine();       // extracting the actual value --> banana
+//                        System.out.println("GOing to read time varaible size");
+//                        String sizeOfTimeVariable = in.readLine();// skipping size of time variable  --> $2
+//                        System.out.println("Time variable size is "+sizeOfTimeVariable);
+//                    if(sizeOfTimeVariable !=null && sizeOfTimeVariable.startsWith("$"))
+//
+//                    {
+//                        in.readLine();                      // skipping  time variable --> px
+//                        in.readLine();                      // skipping size of time in millisecond --> $3
+//
+//                        try{
+//                            long expTime= Long.parseLong(in.readLine());   // extracting the actual time
+//                            GetSet.setKeyWithExpiry(map,key,value,expTime);
+//                        }catch (Exception e){
+//                            System.out.println(e.getMessage());
+//                        }
+//                    }
+//                    else{
+//                        System.out.println("No valid expiry provided");
+//                        GetSet.setKeyWithExpiry(map,key,value,-1);
+//                    }
+//                        clientSocket.getOutputStream().write("+OK\r\n".getBytes());
+//                        clientSocket.getOutputStream().flush();
+//                }
+                else if ("SET".equalsIgnoreCase(inputLine)) {
+                    in.readLine(); // Read size of key
+                    String key = in.readLine(); // Read key
+                    in.readLine(); // Read size of value
+                    String value = in.readLine(); // Read value
 
-                    {
-                        in.readLine();                      // skipping  time variable --> px
-                        in.readLine();                      // skipping size of time in millisecond --> $3
+                    System.out.println("GOing to read time variable size");
+                    String sizeOfTimeVariable = in.readLine(); // Read size of expiry command
+                    System.out.println("time varaible size is "+sizeOfTimeVariable);
+                    // Default expiry time
+                    long expiryTime = -1;
 
-                        try{
-                            long expTime= Long.parseLong(in.readLine());   // extracting the actual time
-                            GetSet.setKeyWithExpiry(map,key,value,expTime);
-                        }catch (Exception e){
-                            System.out.println(e.getMessage());
+                    if (sizeOfTimeVariable != null && sizeOfTimeVariable.startsWith("$")) {
+                        in.readLine(); // Read the 'px' command
+                        in.readLine(); // Read size of expiry time in milliseconds
+                        try {
+                            expiryTime = Long.parseLong(in.readLine()); // Read actual expiry time
+                        } catch (Exception e) {
+                            System.out.println("Error parsing expiry time: " + e.getMessage());
                         }
+                    } else {
+                        System.out.println("No valid expiry provided");
                     }
-                    else{
-//                        System.out.println("Yes no time expiry provided");
-                        GetSet.setKeyWithExpiry(map,key,value,-1);
+
+                    GetSet.setKeyWithExpiry(map, key, value, expiryTime);
+                    clientSocket.getOutputStream().write("+OK\r\n".getBytes());
+                    clientSocket.getOutputStream().flush();
+                }
+                else if ("GET".equalsIgnoreCase(inputLine)) {
+                    in.readLine(); // Read size of key
+                    String key = in.readLine(); // Read key
+
+                    Object value = GetSet.getValue(map, key);
+                    System.out.println("value is " + value);
+
+                    if (value != null) {
+                        clientSocket.getOutputStream().write(
+                                String.format("$%d\r\n%s\r\n", value.toString().length(), value).getBytes());
+                    } else {
+                        clientSocket.getOutputStream().write("$-1\r\n".getBytes()); // Correctly formatted null response
                     }
-                        clientSocket.getOutputStream().write("+OK\r\n".getBytes());
-                        clientSocket.getOutputStream().flush();
+                    clientSocket.getOutputStream().flush();
                 }
 
-                else if("GET".equalsIgnoreCase(inputLine)){
-                    in.readLine();
-                    String key = in.readLine();
-                    Object value = GetSet.getValue(map,key);
-                    if(value!=null){
-                        clientSocket.getOutputStream().write(
-                                String.format("$%d\r\n%s\r\n",value.toString().length(),value).getBytes());
-                        clientSocket.getOutputStream().flush();
-                    }
-                    // if entry not found in map send null to client
-                    else{
-                        clientSocket.getOutputStream().write(
-                                String.format("$%d\r\n",-1).getBytes()
-                        );
-                        clientSocket.getOutputStream().flush();
-                    }
-//                    System.out.println(map);
-                }
+//                else if("GET".equalsIgnoreCase(inputLine)){
+//                    in.readLine();
+//                    String key = in.readLine();
+//
+//                    Object value = GetSet.getValue(map,key);
+//                    System.out.println("value is "+value);
+//                    if(value!=null){
+//                        clientSocket.getOutputStream().write(
+//                                String.format("$%d\r\n%s\r\n",value.toString().length(),value).getBytes());
+//                        clientSocket.getOutputStream().flush();
+//                    }
+//                    // if entry not found in map send null to client
+//                    else{
+//                        clientSocket.getOutputStream().write(
+//                                String.format("$%d\r\n",-1).getBytes()
+//                        );
+//                        clientSocket.getOutputStream().flush();
+//                    }
+////                    System.out.println(map);
+//                }
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
