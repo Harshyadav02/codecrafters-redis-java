7b36bfd0d02de8c8e0812266d641ff2aa40203b7
harshyadav02 <harshyadav5736@gmail.com>
2024-11-04 10:42:39 +0530

    implemented seprated method for each response

--- src/main/java/Replication/HandShake.java
+++ src/main/java/Replication/HandShake.java
@@ -6,40 +6,57 @@ import java.net.Socket;
 
 public class HandShake {
 
-    public void threeWayHandshake(String host, int port, int replicaPort) {
+    // Method to handle the replica handshake
+    public void replicaHandshake(String host, int port, int replicaPort) {
         try (Socket masterSocket = new Socket(host, port)) {
-            
-            // Sending the PING command as a RESP Array: *1\r\n$4\r\nPING\r\n
-            String pingCommand = "*1\r\n$4\r\nPING\r\n";
-            
-            String replconfListeningPortCommand = String.format("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n", String.valueOf(replicaPort).length(), replicaPort);
-            
-            // Second REPLCONF: REPLCONF capa psync2
-            String replconfCapaCommand = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
-            String PSYNC = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
-            OutputStream out = masterSocket.getOutputStream();
-            
+
             // Send the PING command
-            out.write(pingCommand.getBytes());
-            Thread.sleep(20);   
+            sendPingCommand(masterSocket.getOutputStream());
 
-            // Send the first REPLCONF command (listening-port)
-            out.write(replconfListeningPortCommand.getBytes());
-            Thread.sleep(20);  // 
+            // Send the REPLCONF listening-port command
+            sendReplconfListeningPortCommand(masterSocket.getOutputStream(), replicaPort);
 
-            // Send the second REPLCONF command (capa psync2)
-            out.write(replconfCapaCommand.getBytes());
-            Thread.sleep(20);
-            out.write(PSYNC.getBytes());
-            // Ensure all commands are sent
-            out.flush();
-            Thread.sleep(20);
-        } catch (InterruptedException e) {
-            
-            e.printStackTrace();
-        }
-        catch (IOException e) {
+            // Send the REPLCONF capa psync2 command
+            sendReplconfCapaCommand(masterSocket.getOutputStream());
+
+            // Send the PSYNC command
+            sendPsyncCommand(masterSocket.getOutputStream());
+
+        } catch (IOException | InterruptedException e) {
             System.out.println("Error during handshake: " + e.getMessage());
-        } 
+        }
+    }
+
+    // Master handshake implementation
+
+    private void sendPingCommand(OutputStream outputStream) throws IOException, InterruptedException {
+        String pingCommand = "*1\r\n$4\r\nPING\r\n";
+        outputStream.write(pingCommand.getBytes());
+        outputStream.flush();
+        Thread.sleep(20);
+    }
+
+    private void sendReplconfListeningPortCommand(OutputStream outputStream, int replicaPort)
+            throws IOException, InterruptedException {
+        String replconfListeningPortCommand = String.format(
+                "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n",
+                String.valueOf(replicaPort).length(), replicaPort);
+        outputStream.write(replconfListeningPortCommand.getBytes());
+        outputStream.flush();
+        Thread.sleep(20);
+    }
+
+    private void sendReplconfCapaCommand(OutputStream outputStream) throws IOException, InterruptedException {
+        String replconfCapaCommand = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+        outputStream.write(replconfCapaCommand.getBytes());
+        outputStream.flush();
+        Thread.sleep(20);
+    }
+
+    private void sendPsyncCommand(OutputStream outputStream) throws IOException, InterruptedException {
+        String psyncCommand = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
+        outputStream.write(psyncCommand.getBytes());
+        outputStream.flush();
+        Thread.sleep(20);
     }
 }
